bfe2b34d058f22027b038471199101a783f1a58d
Bobby Johnson <bobby.johnson@gmail.com>
2010-11-05 06:34:40 -0700

    added about view and menu items for main screen

---
+++ src/UI/Controllers/AboutViewController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace UI
+{
+	public class AboutViewController : UIViewController
+	{
+		public AboutViewController() : base()
+		{
+		}
+		
+		public UIWebView webView;
+		
+		public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+			
+			webView = new UIWebView()
+			{
+				ScalesPageToFit = true
+			};
+			
+			webView.LoadStarted += delegate {
+				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
+			};
+			webView.LoadFinished += delegate {
+				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+			};
+
+			webView.LoadHtmlString(FormatText(), new NSUrl());
+            webView.SizeToFit();
+            webView.Frame = new RectangleF (0, 0, this.View.Frame.Width, this.View.Frame.Height - 44);
+            this.View.AddSubview(webView);
+		}
+
+		private string FormatText()
+		{
+			StringBuilder sb = new StringBuilder();
+            string html_content = "<p>'You can't middle manage your way to innovation.' --Chad Myers</p>";
+			
+			sb.Append("<html><head><meta name=\"viewport\" content=\"width=320\"/>" +
+				"<style>body,b,p,h2{font-family:Helvetica;}</style></head><body>");
+			sb.Append(html_content);
+			sb.Append("</body></html>");
+				
+			return sb.ToString();
+		}
+	}
+}
+
--- src/UI/Controllers/MainMenuViewController.cs
+++ src/UI/Controllers/MainMenuViewController.cs
@@ -8,7 +8,7 @@ namespace UI
 {
 	public class MainMenuViewController : UITableViewController
 	{
-		private List<string> _items;
+		private List<string> items;
 
 		public MainMenuViewController(UITableViewStyle style) : base(style)
 		{
@@ -32,8 +32,95 @@ namespace UI
 			base.ViewDidLoad();
 			
 			NavigationItem.TitleView = new UIImageView(/*UIImage.FromFile("Images/snakeoil.png")*/);
+			
+			items = new List<string>()
+			{
+				"Russell News Feed",
+				"Russell Twitter Feed"
+			};
+			
+			UIBarButtonItem about = new UIBarButtonItem("About", UIBarButtonItemStyle.Bordered, null);
+			about.Clicked += delegate(object sender, EventArgs e) {
+				AboutViewController aboutView = new AboutViewController();
+				aboutView.Title = "About";
+				this.NavigationController.PushViewController(aboutView, true);				
+			};
+			ToolbarItems = new UIBarButtonItem[] {about};
+			
+			TableView.DataSource = new TableViewDataSource(items);
+			TableView.Delegate = new TableViewDelegate(this);
+			TableView.AutoresizingMask = UIViewAutoresizing.FlexibleHeight|UIViewAutoresizing.FlexibleWidth;
+			TableView.BackgroundColor = UIColor.Clear;
+			TableView.Frame = new RectangleF (0, 0, this.View.Frame.Width, this.View.Frame.Height);
+
 		}
 			
 	}
+	
+	public class TableViewDataSource : UITableViewDataSource
+    {
+		private string cellId = "cellid";
+		private List<string> items;
+
+		public TableViewDataSource (List<string> items)
+        {
+			this.items = items;
+        }
+
+        public override int RowsInSection (UITableView tableview, int section)
+        {
+            return items.Count;
+        }
+
+        public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell(cellId);
+            if (cell == null)
+            {
+                cell = new UITableViewCell (UITableViewCellStyle.Default, cellId);
+            }
+            cell.TextLabel.Text = items[indexPath.Row];
+			cell.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+            return cell;
+        }
+    }	
+	
+	public class TableViewDelegate : UITableViewDelegate
+        {
+			private MainMenuViewController controller;
+			
+            public TableViewDelegate(MainMenuViewController controller)
+            {
+				this.controller = controller;
+            }
+
+			public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
+            {
+				try
+				{
+					UITableViewController nextController = null;
+	
+					switch (indexPath.Row)
+					{
+					case 0:
+						//nextController = new BlogItemsViewController(UITableViewStyle.Plain);
+						break;
+					case 1:
+						//nextController = new NewsItemsViewController(UITableViewStyle.Plain);
+						break;
+					default:
+						break;
+					}
+					
+					if (nextController != null)
+						controller.NavigationController.PushViewController(nextController, true);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+        }
+
 }
 
