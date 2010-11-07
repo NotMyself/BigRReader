b941b705c78b7d7742dc93814040af31d7b8f9a2
Bobby Johnson <bobby.johnson@gmail.com>
2010-11-07 13:45:43 -0800

    fixed the scaling issue with a little script injection

--- src/UI/Controllers/PageViewController.cs
+++ src/UI/Controllers/PageViewController.cs
@@ -19,8 +19,8 @@ namespace UI
 		
 		public UIWebView webView;
 		
-		public override void ViewDidLoad()
-        {
+		public override void ViewDidLoad ()
+		{
 			//base.ViewDidLoad();
 			
 			var webFrame = new RectangleF (0, 0, this.View.Frame.Width, this.View.Frame.Height - 44);
@@ -30,17 +30,27 @@ namespace UI
 				ScalesPageToFit = true,
 				AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
 			};
-						
+			
 			webView.LoadStarted += delegate {
 				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
 			};
 			webView.LoadFinished += delegate {
 				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+				webView.EvaluateJavascript (script ());
 			};
             
             this.View.AddSubview(webView);
 			
 			webView.LoadRequest(NSUrlRequest.FromUrl(new NSUrl(page.AbsoluteUri)));
+		}
+		
+		private string script ()
+		{
+			return "var head = document.getElementsByTagName(\"head\")[0];"
+				 + "var element = document.createElement(\"meta\");"
+				 + "element.name = \"viewport\";"
+				 + "element.content = \"width=device-width; initial-scale=2.90; maximum-scale=2.90;\";"
+				 + "head.appendChild(element);";	
 		}
 	}
 }
