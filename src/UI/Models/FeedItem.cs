ce082487258a46c7db220ecf503d832b69009c50
Bobby Johnson <bobby.johnson@gmail.com>
2010-12-06 12:51:04 -0800

    added additional date string check in the date parsing logic for feed item

--- src/UI/Models/FeedItem.cs
+++ src/UI/Models/FeedItem.cs
@@ -16,13 +16,16 @@ namespace UI
 			}	
 		}
 		
-		private DateTime GetDate(string value) 
+		private DateTime GetDate (string value)
 		{
 			//Thu, 26 Aug 2010 05:00:00 PDT
 			//Thu, 04 Nov 2010 16:44:08 +0000
 			
-			if(value.EndsWith("PDT")) 
-				return DateTime.ParseExact(value.Substring(5), "dd MMM yyyy HH:mm:ss PDT", null);
+			if (value.EndsWith ("PDT"))
+				return DateTime.ParseExact (value.Substring (5), "dd MMM yyyy HH:mm:ss PDT", null);
+			
+			if (value.EndsWith ("PST"))
+				return DateTime.ParseExact (value.Substring (5), "dd MMM yyyy HH:mm:ss PST", null);
 			
 			return DateTime.ParseExact(value, "ddd, dd MMM yyyy HH:mm:ss zzz", null);
 		}
